a96513a8f363612ec845204055110809e448023b
Mameng57 <takiya2888157@gmail.com>
2021-07-15 14:02:16 +0700

    split transaction to a new widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -24,28 +24,9 @@ class TransactionList extends StatelessWidget {
         controller: _scrollController,
         itemCount: transactions.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 4,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 35,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: FittedBox(
-                    child: Text("${transactions[index].amount.toStringAsFixed(3)}",
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                  ),
-                ),
-              ),
-              title: Text("${transactions[index].title}", style: const TextStyle(fontWeight: FontWeight.bold),),
-              subtitle: Text("${DateFormat('EEE - dd MMMM yyyy - HH:mm').format(transactions[index].date)}"),
-              trailing: IconButton(
-                icon: Icon(Icons.delete, color: Theme.of(context).errorColor,),
-                onPressed: () => deleteTransaction(transactions[index].id),
-              ),
-            ),
+          return TransactionItem(
+            transaction: transactions[index],
+            deleteTransaction: deleteTransaction,
           );
         },
       ),
